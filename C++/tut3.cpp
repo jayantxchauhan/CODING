7ead214664c78a670f626a831ad296f8490349a4
JAYANT CHAUHAN <jayantchauhan2005@gmail.com>
2023-12-18 20:47:11 +0530

    Initial commit

---
+++ C++/tut3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//This Program is Created By Jayant
+/*this
+is
+a 
+multiline 
+comment 
+*/
+int main(){
+    int sum = 6;
+    cout<< "Hello World"<< sum;
+    return 0;
+}
